2246b9f9260661c20771d4f526816abe7429ab6d
Thomas Ortiz <thomasortiz95@gmail.com>
2021-03-24 11:43:46 -0400

    export service

--- src/app/public/public_api.ts
+++ src/app/public/public_api.ts
@@ -1,3 +1,4 @@
 export * from './modules/autonumeric/autonumeric-options-provider';
 export * from './modules/autonumeric/autonumeric-options';
 export * from './modules/autonumeric/autonumeric.module';
+export * from './modules/autonumeric/autonumeric-config.service';
