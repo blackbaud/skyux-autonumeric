bbceeff9763bc54cb9b1bf358e60f7b3acec3b53
Thomas Ortiz <thomasortiz95@gmail.com>
2021-04-18 14:22:45 -0400

    Removed mocked i18n service

--- src/app/public/modules/autonumeric/autonumeric-config.service.ts
+++ src/app/public/modules/autonumeric/autonumeric-config.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Options as AutonumericOptions } from 'autonumeric';
-import { SkyCurrencyFormat, SkyI18nSkyCurrencyFormatService } from './MOCK-currency-format.service';
+import {
+  Injectable
+} from '@angular/core';
+
+import {
+  Options as AutonumericOptions
+} from 'autonumeric';
+
+import {
+  SkyI18nCurrencyFormat,
+  SkyI18nCurrencyFormatService
+} from '@skyux/i18n';
 
 type IsoCodeAndLocale = { isoCode: string; locale: string };
 
@@ -13,31 +20,18 @@ type IsoCodeAndLocale = { isoCode: string; locale: string };
   providedIn: 'root'
 })
 export class SkyAutonumericConfigService {
-  constructor(private currencyFormatService: SkyI18nSkyCurrencyFormatService) { }
+  constructor(private currencyFormatService: SkyI18nCurrencyFormatService) { }
 
   /**
    * Creates an Autonumeric Config from a Locale and Currency Code.
    * http://autonumeric.org/guide
    * @param params optional params object
-   * @param params.isoCode the ISO 4217 Code. Defaults to `USD`.
-   * @param params.locale the Locale. Defaults to `SkyAppLocaleProvider.getLocaleInfo`.
+   * @param params.isoCode the ISO 4217 Code.
+   * @param params.locale the Locale.
    */
   public getAutonumericConfig(params?: Partial<IsoCodeAndLocale>): AutonumericOptions {
-    const format = this.currencyFormatService.getCurrencyFormat(params);
+    const format = this.currencyFormatService.getCurrencyFormat(params.isoCode, params.locale);
     return this.mapFromFormatToAutonumericOptions(format);
-  }
-
-  /**
-   * Creates an Observable of Autonumeric Config from a Locale and Currency Code.
-   * http://autonumeric.org/guide
-   * @param params optional params object
-   * @param params.isoCode the ISO 4217 Code. Defaults to `USD`.
-   * @param params.locale the Locale. Defaults to `SkyAppLocaleProvider.getLocaleInfo`
-   */
-  public getAutonumericConfigAsync(params?: Partial<IsoCodeAndLocale>): Observable<AutonumericOptions> {
-    return this.currencyFormatService.getCurrencyFormatAsync(params).pipe(
-      map(config => this.mapFromFormatToAutonumericOptions(config))
-    );
   }
 
   /**
@@ -47,16 +41,16 @@ export class SkyAutonumericConfigService {
    * @see [skyux-autonumeric](https://github.com/blackbaud/skyux-autonumeric)
    * @see [AutoNumeric.js](http://autonumeric.org/guide)
    */
-  private mapFromFormatToAutonumericOptions(format: SkyCurrencyFormat) {
+  private mapFromFormatToAutonumericOptions(format: SkyI18nCurrencyFormat): AutonumericOptions {
     const options: AutonumericOptions = {
       currencySymbol: format.symbol,
-      currencySymbolPlacement: format.symbolLocation,
+      currencySymbolPlacement: format.symbolLocation === 'suffix' ? 's' : 'p',
       decimalCharacter: format.decimalCharacter,
       decimalPlaces: format.precision,
       digitGroupSeparator: format.groupCharacter
     };
 
-    // Autonumeric does not support the same character being used for decimal and group separators so override.
+    // Autonumeric does not support the same character being used for decimal and group separators.
     if (options.decimalCharacter === options.digitGroupSeparator) {
       options.decimalCharacter = '.';
       options.digitGroupSeparator = ',';
