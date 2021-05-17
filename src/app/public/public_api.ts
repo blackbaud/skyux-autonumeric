c77f2a0fd5bea179bc5aa4c7043b0db7aa6b259d
Thomas Ortiz <thomasortiz95@gmail.com>
2021-05-16 22:36:19 -0400

    Remove service export

--- src/app/public/public_api.ts
+++ src/app/public/public_api.ts
@@ -1,4 +1,3 @@
 export * from './modules/autonumeric/autonumeric-options-provider';
 export * from './modules/autonumeric/autonumeric-options';
 export * from './modules/autonumeric/autonumeric.module';
-export * from './modules/autonumeric/autonumeric-config.service';
