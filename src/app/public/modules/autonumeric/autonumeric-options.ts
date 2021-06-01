81f91076b64c684150ec6e150e5de770627ece53
Trevor Burch <Trevor.Burch@blackbaud.com>
2021-06-01 15:43:05 -0400

    Add code examples and make docs tweaks

--- src/app/public/modules/autonumeric/autonumeric-options.ts
+++ src/app/public/modules/autonumeric/autonumeric-options.ts
@@ -3,7 +3,7 @@ import AutoNumeric, {
 } from 'autonumeric';
 
 /**
- * Specifies custom options to provide directly to the underlying [`autoNumeric` utility](https://github.com/autoNumeric/autoNumeric).
+ * Specifies custom options to provide directly to the underlying [autoNumeric utility](https://github.com/autoNumeric/autoNumeric).
  * The value can be set to `string`, which is an alias that represents
  * a [set of predefined set of options](https://github.com/autoNumeric/autoNumeric#predefined-options)
  * for a currency preset or a language preset, or `Options`, which is a
